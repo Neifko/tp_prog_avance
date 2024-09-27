508c7f1953838f8e2e1927bd5de14049f131dc42
Victor G <20vg01@gmail.com>
2024-09-27 09:54:57 +0200

    commit initial

---
+++ docs/rapport1.md
@@ -0,0 +1,12 @@
+# Programmation parallèle sur machine à mémoire partagée
+
+> GUILLERAY Victor  
+> INF3FA  
+
+## TP1
+
+On commence par la conception,
+Diagramme et analyse disponible dans le dossier conception.
+
+
+
---
+++ src/main/java/org/example/UneFenetre.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.awt.*;
+import javax.swing.*;
+
+class UneFenetre extends JFrame
+{
+    UnMobile sonMobile;
+    private final int LARG=400, HAUT=250;
+
+    public UneFenetre()
+    {
+        // TODO
+        // ajouter sonMobile a la fenetre
+        // creer une thread laThread avec sonMobile
+        // afficher la fenetre
+        // lancer laThread
+    }
+}
