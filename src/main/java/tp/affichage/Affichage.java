ebbbae5f539aff3efa2939faa0310bbada5ef6be
Victor G <20vg01@gmail.com>
2024-10-25 09:04:09 +0200

    TP4 : Utilisation des semaphore pour la section critique

--- src/main/java/tp/affichage/Affichage.java
+++ src/main/java/tp/affichage/Affichage.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.BufferedReader;
 import java.lang.String;
+import java.util.concurrent.Semaphore;
 
 class Exclusion {
 };
@@ -16,21 +17,26 @@ public class Affichage extends Thread {
 
     static Exclusion exclusionMutuelle = new Exclusion();
 
+    static semaphoreBinaire sem = new semaphoreBinaire(1);
+
     public Affichage(String txt) {
         texte = txt;
     }
 
     public void run() {
 
-        synchronized (System.out) { //section critique
-            for (int i = 0; i < texte.length(); i++) {
-                System.out.print(texte.charAt(i));
-                try {
-                    sleep(100);
-                } catch (InterruptedException e) {
-                }
-                ;
+        sem.syncWait();
+//        System.out.println("J'entre en section critique");
+        for (int i = 0; i < texte.length(); i++) {
+            System.out.print(texte.charAt(i));
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
             }
+            ;
         }
+//        System.out.println("Je sors de section critique");
+        sem.syncSignal();
+
     }
 }
