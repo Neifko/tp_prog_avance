2f3259a7b88ab80b72bc96e2993c07cf15ad0130
Victor G <20vg01@gmail.com>
2025-02-13 21:18:04 +0100

    tentative avec gpu

---
+++ src/main/java/tp/picuda/MonteCarloPiKernel.cu
@@ -0,0 +1,20 @@
+#include <curand_kernel.h>
+
+extern "C"
+__global__ void monteCarloPi(double *count, int totalIterations) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx >= totalIterations) return;
+
+    curandState state;
+    curand_init(idx, 0, 0, &state);
+
+    double x = curand_uniform(&state);
+    double y = curand_uniform(&state);
+
+    if (x * x + y * y <= 1.0) {
+        count[idx] = 1.0;  // Stocke les hits de chaque thread dans le tableau
+    } else {
+        count[idx] = 0.0;
+    }
+}
